a44c7e160c285c04dfa6d8c711474f30ad27cfbb
Simon Fernandes <fernandes.0911@apside-groupe.com>
2020-01-02 10:51:49 +0100

    ckeditor

--- GDGLeMans-WebApp/src/environments/environment.ts
+++ GDGLeMans-WebApp/src/environments/environment.ts
@@ -1,14 +1,14 @@
 
 export const environment = {
-  // production: false,
-  // issuer: "http://localhost:8080/auth/realms/GDG-Le-Mans",
-  // clientId: "gdg-le-mans",
-  // scope: 'openid profile email',
-  // apiRootUrl: "https://localhost:44324/api/"
-  production: true,
-  issuer: "https://oauth.apsidetop-devel.net/auth/realms/GDG-Le-Mans",
+  production: false,
+  issuer: "http://localhost:8080/auth/realms/GDG-Le-Mans",
   clientId: "gdg-le-mans",
   scope: 'openid profile email',
-  apiRootUrl: "https://gdglemans.apsidetop-devel.net/api"
+  apiRootUrl: "https://localhost:44324/api/"
+  // production: true,
+  // issuer: "https://oauth.apsidetop-devel.net/auth/realms/GDG-Le-Mans",
+  // clientId: "gdg-le-mans",
+  // scope: 'openid profile email',
+  // apiRootUrl: "https://gdglemans.apsidetop-devel.net/api"
 
 };
